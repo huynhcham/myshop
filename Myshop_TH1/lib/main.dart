7bf3e6ca566211b1752efa828006949b352e8bac
huynhcham <chamb1900233@student.ctu.edu.vn>
2022-09-18 19:40:50 +0700

    xay dung trang tong quan san pham

--- Myshop_TH1/lib/main.dart
+++ Myshop_TH1/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'ui/products/products_manager.dart';
+import 'ui/products/product_detail_screen.dart';
+import 'ui/products/products_overview_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,8 +24,8 @@ class MyApp extends StatelessWidget {
           secondary: Colors.deepOrange,
         ),
       ),
-      home: Container(
-        color: Colors.green,
+      home: const SafeArea(
+        child: ProductsOverviewScreen(),
       ),
     );
   }
